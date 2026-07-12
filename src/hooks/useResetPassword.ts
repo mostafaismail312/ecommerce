5caceafd095e9503e94b3288f6f5474d016b2549
Ameera <amiraelalfe3@gmail.com>
2026-07-12 20:55:51 +0300

    reset verify

--- src/hooks/useResetPassword.ts
+++ src/hooks/useResetPassword.ts
@@ -1,16 +1,19 @@
 import { useForm } from "react-hook-form";
 import type { ResetPasswordFormData } from "../types/auth";
 import { resetPassword } from "../api/auth.api";
+import { useNavigate } from "react-router-dom";
 
 
 export const useResetPassword = () => {
   const methods = useForm<ResetPasswordFormData>();
+  const navigate = useNavigate();
 
   const onSubmit = async (data: ResetPasswordFormData) => {
     try {
       const response = await resetPassword(data);
 
       console.log(response.data);
+        navigate("/login");
     } catch (error) {
       console.log(error);
     }
