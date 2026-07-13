b10e112e30b3d3c2e6fb4004f4508e998aaf2177
Ameera <amiraelalfe3@gmail.com>
2026-07-13 15:00:08 +0300

    nnn

--- src/Layouts/MainLayout/AuthLayout.tsx
+++ src/Layouts/MainLayout/AuthLayout.tsx
@@ -1,12 +1,13 @@
 import Footer from "../../components/Footer/Footer";
 import {Outlet} from "react-router"
+import NavBar from "../../components/NavBar/NavBar";
 
 export default function AuthLayout() {
   return (
     <>
-  
+  <NavBar/>
     <Outlet />
-  
+   <Footer/>
     </>
   )
 }
